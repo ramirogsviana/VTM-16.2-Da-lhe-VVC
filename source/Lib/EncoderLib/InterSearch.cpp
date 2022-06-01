1b3711acf6c7508e9ea60c730544c717a31d9a2d
ramirogsviana <106404791+ramirogsviana@users.noreply.github.com>
2022-06-01 17:57:19 -0300

    VTM 16.2 original

--- source/Lib/EncoderLib/InterSearch.cpp
+++ source/Lib/EncoderLib/InterSearch.cpp
@@ -1,3 +1,5 @@
+//Ramiro: teste de alteração pelo GitHub
+
 /* The copyright in this software is being made available under the BSD
  * License, included below. This software may be subject to other third party
  * and contributor rights, including patent rights, and no such rights are
